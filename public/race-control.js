829f62ed37697efe6b72750c5b16fe096d4974a7
karl-hendrikkahn <karlhendrikkahn@gmail.com>
2024-08-29 15:27:58 +0300

    race-control improvements

--- public/race-control.js
+++ public/race-control.js
@@ -1,15 +1,20 @@
 const socket = io();
 const accessForm = document.getElementById('access-form');
 const accessKeyInput = document.getElementById('access-key');
+const newSessionDiv = document.getElementById('session-content')
+const newSessionButton = document.getElementById('see-new-session')
 const contentDiv = document.getElementById('content');
 const contentDivAfterStart = document.getElementById('content-after-start')
+const contentDivAfterFinish = document.getElementById('content-after-finish')
 const errorMessage = document.getElementById('error-message');
 const startRaceButton = document.getElementById('start-race');
 const modeSafeButton = document.getElementById('mode-safe');
 const modeHazardButton = document.getElementById('mode-hazard');
 const modeDangerButton = document.getElementById('mode-danger');
 const endRaceButton = document.getElementById('end-race');
+const finishSessionButton = document.getElementById('end-session')
 const raceModeDisplay = document.getElementById('race-mode-start');
+const raceInformationDiv = document.getElementById('race-information')
 
 // Handle access key submission
 accessForm.addEventListener('submit', function (event) {
@@ -22,18 +27,33 @@ accessForm.addEventListener('submit', function (event) {
 socket.on('key-validation', function (response) {
     if (response.success) {
         accessForm.style.display = 'none';
-        contentDiv.style.display = 'block';
+        newSessionDiv.style.display = 'block'
     } else {
         errorMessage.textContent = 'Invalid access key. Please try again.';
     }
+});
+
+//TODO: currently just shows the race info, needs to be changed to show driver and what car they drive
+socket.on('display-race', (race) => {
+    raceInformationDiv.innerHTML = '';
+
+    for (const [key, value] of Object.entries(race)) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = `${key}: ${value}`;
+        raceInformationDiv.appendChild(paragraph);
+    }
+})
+
+newSessionButton.addEventListener('click', () => {
+    socket.emit('start-session');
+    contentDiv.style.display = 'block';
+    newSessionDiv.style.display = 'none'
 });
 
 // Start race
 startRaceButton.addEventListener('click', () => {
     console.log('Start Race button clicked');
     socket.emit('start-race');
-    contentDiv.style.display = 'none'
-    contentDivAfterStart.style.display = 'block'
 });
 
 modeSafeButton.addEventListener('click', () => {
@@ -59,9 +79,44 @@ endRaceButton.addEventListener('click', () => {
     raceModeDisplay.textContent = 'Finish'
     console.log('End Race button clicked');
     socket.emit('end-race');
-
-    // Not tied to the BE 
-    // updateRace(string id)
-
 });
+
+finishSessionButton.addEventListener('click', () => {
+    socket.emit('end-session')
+});
+
+socket.on('race-status', (status) => {
+    console.log('Received race status:', status);
+    // Update the UI with the new race status
+    document.getElementById('race-status-display').innerText = status;
+    if (status === 'No upcoming race found') {
+        contentDiv.style.display = 'block'
+        contentDivAfterStart.style.display = 'none'
+    } else {
+        contentDiv.style.display = 'none'
+        contentDivAfterStart.style.display = 'block'
+        document.getElementById('race-status-display-2').innerText = status;
+    }
+});
+
+socket.on('race-status', (status) => {
+    if (status === 'Race ended') {
+        disableButtons()
+        contentDivAfterFinish.style.display = 'block'
+    }
+})
+
+function disableButtons() {
+    document.getElementById('mode-safe').disabled = true
+    document.getElementById('mode-hazard').disabled = true
+    document.getElementById('mode-danger').disabled = true
+    document.getElementById('end-race').disabled = true
+}
+
+function enableButtons() {
+    document.getElementById('mode-safe').disabled = false
+    document.getElementById('mode-hazard').disabled = false
+    document.getElementById('mode-danger').disabled = false
+    document.getElementById('end-race').disabled = false
+}
 
--- src/js/race-control.js
+++ src/js/race-control.js
@@ -5,7 +5,7 @@ module.exports = (io, socket) => {
     console.log('Setting up race control');
 
     // Handle starting the race
-    socket.on('start-race', () => {
+    socket.on('start-session', () => {
         db.get("SELECT * FROM races WHERE status = '8' LIMIT 1", (err, row) => {
             if (err) {
                 console.error(err.message)
@@ -13,16 +13,24 @@ module.exports = (io, socket) => {
             }
 
             if (row) {
-                console.log('Race started');
+                console.log('Session found');
                 const race = new Race(row)
                 console.log(race)
-                io.emit('race-status', 'Race started');
-                io.emit('race-mode', 'Safe'); // Assuming the race starts in 'Safe' mode
+                io.emit('display-race', race)
             } else {
                 io.emit('race-status', 'No upcoming race found')
             }
         })
     });
+
+    socket.on('start-race', () => {
+        io.emit('race-status', 'Race started');
+        io.emit('race-mode', 'Safe');
+    })
+
+    socket.on('end-session', () => {
+        //TODO: if this is called, prepare the next session. Set the previous race to inactive in DB, set the next race status to 8, set the next queued race status to 4. 
+    })
 
     // Handle changing race mode
     socket.on('change-mode', (mode) => {
